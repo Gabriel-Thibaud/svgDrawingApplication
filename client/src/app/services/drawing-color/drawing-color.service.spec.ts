1284970e5540e5735f8b7552c785bf63d68a5aee
Gabriel-Thibaud <ggabrielthibaud@gmail.com>
2021-02-16 15:52:13 -0500

    ajout du projet sur gitHub

---
+++ client/src/app/services/drawing-color/drawing-color.service.spec.ts
@@ -0,0 +1,12 @@
+// import { TestBed } from '@angular/core/testing';
+
+// import { DrawingColorService } from './drawing-color.service';
+
+// describe('DrawingColorService', () => {
+//   beforeEach(() => TestBed.configureTestingModule({}));
+
+//   it('should be created', () => {
+//     const service: DrawingColorService = TestBed.get(DrawingColorService);
+//     expect(service).toBeTruthy();
+//   });
+// });
---
+++ client/src/app/services/drawing-color/drawing-color.service.ts
@@ -0,0 +1,21 @@
+// import { EventEmitter, Injectable, Output } from '@angular/core'
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class DrawingColorService {
+
+//   communicateWithDrawing: boolean;
+
+//   constructor() {
+//     this.communicateWithDrawing = false;
+//   }
+
+//   @Output() backgroundColor: EventEmitter<string> = new EventEmitter();
+
+//   changeBackgroundColor(): void {
+//     this.communicateWithDrawing = !this.communicateWithDrawing;
+//     this.backgroundColor.emit('black');
+//   }
+
+// }
